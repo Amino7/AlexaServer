8c3cabe198fca415d027ba1ef7238105ebdcb794
Amin U <amin.uzdenov@hotmail.de>
2018-07-27 09:47:41 +0200

    removed <br>

--- apps/test-skill/index.js
+++ apps/test-skill/index.js
@@ -48,7 +48,7 @@ app.intent('sayChickenNumber',
       console.log(APIResponse);
     });
     var number = request.slot('number');
-    response.say('You said you wanted to buy ' + chickens + ' <br>');
+    response.say('You said you wanted to buy ' + chickens + '. '+ APIResponse);
   }
 );
 
